841595022678c4822c4b92f1cf48c9e5f8fcca8a
NDFour <ndfour@foxmail.com>
2018-02-06 15:43:04 +0800

    modefy

--- gooddianying.net/dbtest2.py
+++ gooddianying.net/dbtest2.py
@@ -3,7 +3,7 @@ import pymysql
 def main():
     db=pymysql.connect(host='127.0.0.1',user='root',password='cqmygpython2',db='test',port=3306)
     cur=db.cursor()
-    sql='insert into fortable (id,name) values(001,'my_name')'
+    sql='insert into fortable (id,name) values(001,"my_name")'
     cur.execute(sql)
     db.commit()
     cur.close()
