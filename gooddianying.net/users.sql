c6b8e05bde233ce293f2c1796511c1892103fd52
NDFour <ndfour@foxmail.com>
2018-02-26 18:02:13 +0800

    录入已有 name_dic 数据到 users 表并到处至 users.sql

--- gooddianying.net/users.sql
+++ gooddianying.net/users.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.59, for debian-linux-gnu (i686)
+-- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
 --
 -- Host: localhost    Database: wechatmovie
 -- ------------------------------------------------------
--- Server version	5.5.59-0ubuntu0.14.04.1
+-- Server version	5.7.21-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -27,7 +27,7 @@ CREATE TABLE `users` (
   `target_id` varchar(255) DEFAULT NULL,
   `target_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,6 +36,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'gh_a987c1f298e2','测试账号'),(2,'gh_499743c9649e','一起来电影'),(3,'gh_2a98dd25db1f','文艺的小猪'),(4,'gh_a7d8a272069c','电影假期');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -48,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-02-26 17:53:36
+-- Dump completed on 2018-02-26 18:00:12
