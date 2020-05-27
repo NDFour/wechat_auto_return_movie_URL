2f50d4f41d165d55f994e43c5b7ccfe5fd1ec55f
Lynn <NDFour@foxmail.com>
2020-05-27 17:09:17 +0800

    增加从数据库回复在线播放链接

--- autoResponse/responseFromDB/wxRobot.py
+++ autoResponse/responseFromDB/wxRobot.py
@@ -3,21 +3,11 @@
 from werobot import WeRoBot
 import re
 import configparser
+import sqlite3
 
 
 # 默认启用 Session
 robot = WeRoBot(token='wx123')
-
-# 判断回复用户的方式 （在线 or 网盘）
-reply_info_state = 1
-
-# 在线播放
-baseUrl1 = 'http://tnt1024.com/movie/search/?movie_name='
-# 百度网盘链接
-baseUrl2 = 'http://tnt1024.com/movie/search/?movie_name='
-# 小说URL
-novelUrl1 = ''
-
 
 @robot.subscribe
 def subscribe(message):
@@ -30,195 +20,103 @@ def subscribe(message):
 @robot.text
 def hello(message, session):
     # return '【通知】\n\n    因公众号提供电影服务的服务器压力增加，导致价格上涨。公众号免费提供电影服务，无任何盈利，现需要暂时暂停电影服务。望谅解！\n\n    服务恢复时间请关注本公众号通知！\n\n<a href="https://mp.weixin.qq.com/s/IV9we9FYrPxi5w68fuLb9A">点我留言反馈</a>'
-    # lynn 微信号的 source ID
-    master_root = 'o2NddxHhZloQV55azmx8zVXv9mAQ'
-    # master_root = 'oBo_Y1akWzT_4_i38mzpiRo7z-uo'
 
-    # 返回公众号 target ID
-    if message.content=='showtarget':
-        return message.target
-
-    # 读取配置文件
-    loadConfigMsg = loadConfig()
-
-    # 用户消息纯数字，由掌上大学接管自动回复
+    str_input = message.content.strip()
+    msg = ''
     try:
-    	int(message.content)
-    	return ''
-    except:
-    	pass
+        # 该回复为 电影 ID
+        str_input = int(str_input)
 
-    # 公众号相关接口
-    if message.source == master_root:
-        msg = toolsforAdmin(message.content)
-        if len(msg) > 0:
-            return msg
-
-    # print the name of the movie and source
-    print('《%s》 from [%s]' %(message.content, message.source))
-
-    if (len(message.content) > 30):
-    	return '电影名长度过长，请精简后重新发送。'
-
-    v_name = modefy_name(message.content)
-
-    global reply_info_state
-    articles = []
-    if reply_info_state == 1:
-    	articles = reply_info_bygenurl(v_name)
-    elif reply_info_state == 2:
-    	articles = reply_info_bygenurl(v_name)
-
-    # 如果当天第一次使用,重定向到小说
-    # if 'first' not in session:
-    #     articles = redirettoNovel(articles)
-
-    # List to String
-    rel_str = genHtml(articles)
-
-    # 如果当天第一次使用,重定向到小说
-    # if 'first' not in session:
-    #     rel_str += '\n\n★ 如果点击上方无法跳转到影片搜索页，请尝试重新发送片名'
-    #     session['first'] = True
-
-    return rel_str
+        # 根据 ID 搜索 详情
+        msg = get_by_id(str_input)
+        # print(msg)
+        # print()     
+    except Exception as e:
+        # 该回复为 电影名
+        # 若 将用户发送消息 转为 int 失败，则表示 用户发送的是 片名，而不是 电影 ID
+        str_input = str_input.replace('《', '').replace('》', '')
+        msg = get_rel(str_input)
+        # print(msg)
+        # print()
 
 
-# List to String
-def genHtml(articles):
-    rel_str=''
-    ar_str_cnt=0
-    for ar in articles:
-        ar_str_cnt += 1
-        rel_str+='['+str(ar_str_cnt)+']  '+'<a href="'+ar[-1]+'">'+ar[0]+'</a>'+'\n\n\n'
-
-    return rel_str.strip()
+    return msg
 
 
-# Is first to use  -- Redirect to wx novels
-def redirettoNovel(articles):
-    config=configparser.ConfigParser()
-    config.read("config.ini")
-
-    articles[0][3] = config.get('werobot','novelUrl1')
-    return articles
-
-
-# 替换用户发来的电影名字中的错别字
-def modefy_name(v_name):
-    # 先把电影名字中的特殊符号去除
-    v_name=v_name.replace('《','')
-    v_name=v_name.replace('》','')
-    v_name=v_name.replace('。','')
-
-    return v_name
-
-
-# 构造查询 url 返回给用户
-def reply_info_bygenurl(v_name):
-    out_list=[]
-    global baseUrl1
-    url = baseUrl1 + v_name
-    name='《'+v_name+'》点我在线观看'
-    picurl='https://s1.ax1x.com/2018/08/11/P6L2sU.jpg'
-
-    in_list=[]
-    in_list.append(name)
-    in_list.append(name)
-    in_list.append(picurl)
-    in_list.append(url)
-
-    out_list.append(in_list)
-
-    return out_list
-
-
-# 公众号相关接口
-def toolsforAdmin(content):
-    # Switch reply method
-    if re.match(r'switch [0-9]', content):
-        reply_info_state = int(content[7])
-        msg = writeToConfigFile('reply_info_state', str(reply_info_state))
-        return msg
-
-    # Change baseUrl1
-    elif re.match(r'changebaseurl1 .*', content):
-        global baseUrl1
-        baseUrl1 = content[15:]
-        msg = writeToConfigFile('baseUrl1', baseUrl1)
-        return msg + '\n更改 baseUrl1 成功!'
-
-    # Change baseUrl2
-    elif re.match(r'changebaseurl2 .*', content):
-        global baseUrl2
-        baseUrl2 = content[15:]
-        msg = writeToConfigFile('baseUrl2', baseUrl2)
-        return msg + '\n更改 baseUrl2 成功!'
-
-    # Change novelUrl1
-    elif re.match(r'changenovelurl1 .*', content):
-        global novelUrl1
-        novelUrl1 = content[16:]
-        msg = writeToConfigFile('novelUrl1', novelUrl1)
-        return msg + '\n更改 novelUrl1 成功!'
-
-    # Return content of config.ini
-    elif content == 'showConfig':
-    	msg = showConfig()
-    	return msg
-
-    else:
-        return ''
-
-
-# Read config from config.ini
-def loadConfig():
-    global reply_info_state
-    global baseUrl1
-    global baseUrl2
-    global novelUrl1
-
-    config=configparser.ConfigParser()
-    config.read("config.ini")
-
-    # 各位置adarticles状态开关
+def get_rel(name):
+    # 回复给用户的消息体
+    msg = ''
     try:
-        reply_info_state = config.getint('werobot','reply_info_state')
-        baseUrl1 = config.get('werobot','baseUrl1')
-        baseUrl2 = config.get('werobot','baseUrl2')
-        novelUrl1 = config.get('werobot','novelUrl1')
-    except:
-        return 'config.ini配置文件加载失败'
+        conn = sqlite3.connect('scrapy.sqlite3')
+        cursor = conn.cursor()
 
-    return 'config.ini配置文件加载完毕'
+        sql = "select id,name from movies where name" + " like '%" + name + "%' order by length(name) limit 15"
+        # print(sql)
+        cursor.execute(sql)
+        rel = cursor.fetchall()
+
+        '''
+        搜索「城南旧事」的结果,
+        发送前面编码获得网盘，注册用户直接点击书名查看
+        - - - - - - - - - - - - - - - - - - 
+        [ 103704 ]城南旧事
+        [ 112760 ]城南旧事
+        [ 114159 ]城南旧事
+        - - - - - - - - - - - - - - - - - - 
+        ◎ 参加辣豆瓣每天读书5分钟打卡
+        ◎ 查看书单，发送数字 3 
+        ◎ 加入书友群求助书友。
+        '''
+
+        if len(rel):
+            msg = '搜索 《' + name + '》 的结果，\n发送前面编码获得播放链接。\n- - - - - - - - - - - - - - - - - - \n'
+            for m in rel:
+                msg += '[ ' + str(m[0]) + ' ]' + m[1] + '\n'
+            msg += '- - - - - - - - - - - - - - - - - - \n'
+            msg += '⚠️ 名字可以不完整，但是一定不要有错别字哦 ~'
+
+        else:
+            msg = '你好，没有找到跟《' + name + '》相符合的电影哦\n你可以换一个电影试试~~~\n\n⚠️ 如果片名为纯数字，在发送片名的时候请加上书名号，如《1984》，其中 1984 为片名，否则会搜索不出结果'
+    except Exception as e:
+        # print(e)
+        msg = '你好，没有找到跟《' + name + '》相符合的电影哦\n你可以换一个电影试试~~~'
+    finally:
+        cursor.close()
+        conn.close()
+        # print('finally 这里执行了')
+
+    return msg
 
 
-# 接受两个参数 ： 配置项名称、配置项的值
-def writeToConfigFile(configName,configValue):
-    config=configparser.ConfigParser()
-    config.read('config.ini')
+def get_by_id(id):
+    msg = ''
     try:
-        config.set('werobot',configName,configValue)
-        config.write(open('config.ini','w'))
-    except:
-        return '写入 %s 到配置文件 config.ini 时失败' % configName
-    return '写入 %s 到配置文件 config.ini 成功' % configName
+        conn = sqlite3.connect('scrapy.sqlite3')
+        cursor = conn.cursor()
 
+        sql = "select name,url_m3u8 from movies where id=" + str(id)
+        # print(sql)
+        cursor.execute(sql)
+        rel = cursor.fetchall()
 
-# Return content of config.ini
-def showConfig():
-    global reply_info_state
-    global baseUrl1
-    global baseUrl2
-    global novelUrl1
+        msg = '《' + rel[0][0] + '》'
+        msg += '\n- - - - - - - - - - - - - - - - - - \n'
+        chatper_list = rel[0][1].split('##')
 
-    msg = 'config.ini:\n'
-    msg += '\nreply_info_state:' + str(reply_info_state)
-
-    msg += '\n\nbaseUrl1:' + baseUrl1
-    msg += '\nbaseUrl2:' + baseUrl2
-
-    msg += '\n\nnovelUrl1:' + novelUrl1
+        # 从 url_m3u8 中分割每一集
+        for url in chatper_list:
+            if len(url):
+                name_url = url.split('$')
+                msg += '<a href="' + name_url[1] + '">' + name_url[0] + '集</a>\n'
+            else:
+                pass
+    except Exception as e:
+        # print(e)
+        msg = '你好，没有找到 ID 为 ' + str(id) + ' 的影片，请检查你的输入 ~'
+    finally:
+        # print('get_by_id finally 执行了')
+        cursor.close()
+        conn.close()
 
     return msg
 
